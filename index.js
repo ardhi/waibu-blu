32a84aa1e3314857a99df2e03a4885f4dea98555
Ardhi Lukianto <ardhi@lukianto.com>
2025-09-28 21:21:07 +0700

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+/**
+ * Plugin factory
+ *
+ * @param {string} pkgName - NPM package name
+ * @returns {class}
+ */
 async function factory (pkgName) {
   const me = this
 
+  /**
+   * WaibuBlu class
+   *
+   * @class
+   */
   class WaibuBlu extends this.app.pluginClass.base {
     static alias = 'wblu'
     static dependencies = ['waibu-mpa', 'waibu-bootstrap']
